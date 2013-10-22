42fdcf05e0f8e53ad53d24dbadf31bb48780b0c8
Philipp Borgers <borgers@mi.fu-berlin.de>
2013-10-22 12:46:05 +0200

    show missing nodes in console

--- js/loader.js
+++ js/loader.js
@@ -3,6 +3,11 @@ function load_nodes(data, cb) {
   var url = 'http://api.openwifimap.net/view_nodes_spatial?bbox=13.179473876953125,52.45308034523523,13.647079467773438,52.59241215943279'
   d3.json(url, function(json) {
     var data = json.rows;
+
+    var original = data.map(function(d, i){
+      return d.id;
+    }).sort();
+
     for (var i=0; i < data.length; ++i) {
       var entry = data[i].value;
       if ("links" in entry) {
@@ -15,10 +20,23 @@ function load_nodes(data, cb) {
       }
     }
 
+    diff = function(a, b) {
+        return a.filter(function(i) {return !(b.indexOf(i) > -1);});
+    };
+
+
+    var converted = graph.getNodes().map(function(d){ return d.id; }).sort();
+
+    console.log("Missing nodes: " + diff(original, converted));
+    console.log("# of missing nodes: " + diff(original, converted).length);
+
+    var edges = graph.getEdges();
+
+
     cb({
       'meta': {"timestamp": "2013-08-08T14:32:30"},
       'nodes' : graph.getNodes(),
-      'links' : graph.getEdges()
+      'links' : edges
     });
   });
 };
