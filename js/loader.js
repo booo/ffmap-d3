300b3041e04b24343a8553698e476c5f422838ce
Philipp Borgers <borgers@mi.fu-berlin.de>
2013-10-26 00:00:33 +0200

    fix issue #14

--- js/loader.js
+++ js/loader.js
@@ -1,42 +1,42 @@
 function load_nodes(data, cb) {
-  var graph = new SimpleGraph();
   var url = 'http://api.openwifimap.net/view_nodes_spatial?bbox=13.179473876953125,52.45308034523523,13.647079467773438,52.59241215943279'
   d3.json(url, function(json) {
+
     var data = json.rows;
+    var links = [];
+    var lookup = {};
+    var nodes = [];
 
-    var original = data.map(function(d, i){
-      return d.id;
-    }).sort();
+    data.forEach(function(d, i){
+        nodes.push({
+          id: d.id,
+          flags: {
+            cleint: false
+          }
+        });
+        lookup[d.id] = nodes[i];
+      });
 
-    for (var i=0; i < data.length; ++i) {
-      var entry = data[i].value;
-      if ("links" in entry) {
-        for (var j=0; j < entry.links.length; j++) {
-          var link = entry.links[j];
-          graph.addEdge(entry, link.id, link)
-        }
-      } else {
-        graph.addSingleNode(entry, false);
+    data.forEach(function(source, i){
+      //check if there a links at all
+      if(source.value.links) {
+        source.value.links.forEach(function(target){
+          //check if target exists
+          if(lookup[target.id]) {
+            links.push({
+              'id': source.id + ',' + target.id,
+              'source': nodes[i],
+              'target': lookup[target.id],
+            });
+          }
+        });
       }
-    }
-
-    diff = function(a, b) {
-        return a.filter(function(i) {return !(b.indexOf(i) > -1);});
-    };
-
-
-    var converted = graph.getNodes().map(function(d){ return d.id; }).sort();
-
-    console.log("Missing nodes: " + diff(original, converted));
-    console.log("# of missing nodes: " + diff(original, converted).length);
-
-    var edges = graph.getEdges();
-
+    });
 
     cb({
       'meta': {"timestamp": "2013-08-08T14:32:30"},
-      'nodes' : graph.getNodes(),
-      'links' : edges
+      'nodes': nodes,
+      'links' : links
     });
   });
 };
