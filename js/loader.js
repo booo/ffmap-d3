54daf30c6c84a966d9181f32bfefd5a263f03a6a
Philipp Borgers <borgers@mi.fu-berlin.de>
2013-10-26 00:00:33 +0200

    fix issue #14
    Conflicts:
    	js/loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,24 +1,42 @@
 function load_nodes(data, cb) {
-  var graph = new SimpleGraph();
   var url = 'http://api.openwifimap.net/view_nodes_spatial?bbox=13.179473876953125,52.45308034523523,13.647079467773438,52.59241215943279'
   d3.json(url, function(json) {
+
     var data = json.rows;
-    for (var i=0; i < data.length; ++i) {
-      var entry = data[i].value;
-      if ("links" in entry) {
-        for (var j=0; j < entry.links.length; j++) {
-          var link = entry.links[j];
-          graph.addEdge(entry, link.id, link)
-        }
-      } else {
-        graph.addSingleNode(entry, false);
+    var links = [];
+    var lookup = {};
+    var nodes = [];
+
+    data.forEach(function(d, i){
+        nodes.push({
+          id: d.id,
+          flags: {
+            cleint: false
+          }
+        });
+        lookup[d.id] = nodes[i];
+      });
+
+    data.forEach(function(source, i){
+      //check if there a links at all
+      if(source.value.links) {
+        source.value.links.forEach(function(target){
+          //check if target exists
+          if(lookup[target.id]) {
+            links.push({
+              'id': source.id + ',' + target.id,
+              'source': nodes[i],
+              'target': lookup[target.id],
+            });
+          }
+        });
       }
-    }
+    });
 
     cb({
       'meta': {"timestamp": "2013-08-08T14:32:30"},
-      'nodes' : graph.getNodes(),
-      'links' : graph.getEdges()
+      'nodes': nodes,
+      'links' : links
     });
   });
 };
